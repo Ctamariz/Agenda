3eb68820ed121327176aaa66b16455bac58cc17b
Cvande1487 <titovdv56@gmail.com>
2017-04-27 11:32:11 -0600

    incorporacion de contactos en agenda

--- app/src/main/java/com/example/charls/agenda/Clases/Agenda.java
+++ app/src/main/java/com/example/charls/agenda/Clases/Agenda.java
@@ -1,9 +1,11 @@
 package com.example.charls.agenda.Clases;
 
+import java.util.ArrayList;
+
 /**
  * Created by cvande on 27/4/2017.
  */
 
 public class Agenda {
-
+    ArrayList<Contacto>contactos=new ArrayList<Contacto>();
 }
