a7c957746d376c8d2a88ffb557615f0fc782f22c
Ctamariz <you@example.com>
2017-05-01 14:11:17 -0600

    get andset

--- app/src/main/java/com/example/charls/agenda/Clases/Agenda.java
+++ app/src/main/java/com/example/charls/agenda/Clases/Agenda.java
@@ -8,4 +8,8 @@ import java.util.ArrayList;
 
 public class Agenda {
     ArrayList<Contacto>contactos=new ArrayList<Contacto>();
+
+
+
+
 }
---
+++ app/src/main/java/com/example/charls/agenda/Clases/Grupo.java
@@ -0,0 +1,26 @@
+package com.example.charls.agenda.Clases;
+
+/**
+ * Created by charls on 05-01-17.
+ */
+
+public class Grupo {
+    private int idGrupo;
+    private String nombre;
+
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
